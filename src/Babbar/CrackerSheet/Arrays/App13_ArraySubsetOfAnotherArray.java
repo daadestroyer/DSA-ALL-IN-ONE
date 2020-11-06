7a0c896922580109b8a8a08515fd3733c1337268
kool nigam <nigamshubham2000@gmail.com>
2020-11-06 11:41:34 +0530

    4 Questions completed from Arrays on 5|Nov|2020...

---
+++ src/Babbar/CrackerSheet/Arrays/App13_ArraySubsetOfAnotherArray.java
@@ -0,0 +1,41 @@
+package Babbar.CrackerSheet.Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App13_ArraySubsetOfAnotherArray {
+    static boolean arraySubsetOfAnotherArray(int[] a1, int[] a2) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < a1.length; i++) {
+            list.add(a1[i]);
+        }
+
+        for (int i = 0; i < a2.length; i++) {
+            if (!list.contains(a2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int s1 = sc.nextInt();
+            int s2 = sc.nextInt();
+            int[] a1 = new int[s1];
+            int[] a2 = new int[s2];
+
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = sc.nextInt();
+            }
+            for (int i = 0; i < a2.length; i++) {
+                a2[i] = sc.nextInt();
+            }
+            String res = arraySubsetOfAnotherArray(a1,a2) ? "Yes":"No";
+            System.out.println(res);
+
+        }
+    }
+}
